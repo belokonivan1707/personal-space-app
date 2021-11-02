7c790176cf6d9d91635cdeffa45e7c2d9bec1c84
Ivan Bilokon <Belokonivan1707@gmail.com>
2021-11-02 17:08:04 +0200

    regular commit

--- src/components/cards/cinema-card/cinema-card.tsx
+++ src/components/cards/cinema-card/cinema-card.tsx
@@ -16,6 +16,8 @@ export interface Props {
 const CinemaCard = ({ props }: Props) => {
   const dispatch = useDispatch();
 
+  // const data = useFuckingData();
+
   const { id, title, director, country, image, year, rating, favorite } = props;
 
   const array = [1, 2, 3, 4, 5];
